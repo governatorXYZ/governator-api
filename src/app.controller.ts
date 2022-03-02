f4115a5e8583350b43f38825bf004e973a6db4a4
tikisailor <40813246+tikisailor@users.noreply.github.com>
2022-03-02 18:55:12 +0800

    basic mongo crud setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, MessageEvent, Post, Sse } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    MessageEvent,
+    Param,
+    Post, Put,
+    Sse,
+} from '@nestjs/common';
 import { Observable, Subject } from 'rxjs';
-import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
-import { DummyDataDto } from './app.dtos';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiParam } from '@nestjs/swagger';
+import { DummyDataDto } from './dtos/passTrough.dtos';
+import { CreatePollDto, UpdatePollDto } from './dtos/poll.dtos';
+import { MongoService } from './services/mongo.service';
+
 // import * as cron from 'cron';
 
 // set cron job to add messages to event stream
@@ -14,7 +25,7 @@ import { DummyDataDto } from './app.dtos';
 @Controller()
 export class AppController {
     private readonly eventStream: Subject<any>;
-    constructor() {
+    constructor(protected mongoService: MongoService) {
         this.eventStream = new Subject();
     }
 
@@ -24,13 +35,81 @@ export class AppController {
     }
 
     // @eslint-ignore
-    @Post('create-poll')
+    @Post('pass-through')
     @ApiOperation({
-        description: 'Endpoint to create new poll',
+        description: 'Pass through endpoint for testing',
     })
-    @ApiOkResponse({ description: 'puts params into event stream', type: DummyDataDto })
+    @ApiOkResponse({ description: 'Puts input params into event stream', type: DummyDataDto })
     updateStream(@Body() params: DummyDataDto): void {
         // console.log(params);
         this.eventStream.next(params);
     }
+
+    // @eslint-ignore
+    @Post('poll/create')
+    @ApiOperation({ description: 'Create a new poll' })
+    @ApiCreatedResponse({ description: 'The record has been successfully created.', type: CreatePollDto })
+    async createPoll(@Body() params: CreatePollDto): Promise<void> {
+        await this.mongoService.createPoll(params);
+    }
+
+    @Get('poll/list')
+    async fetchAllPolls() {
+        return await this.mongoService.fetchAllPolls();
+    }
+
+    @Get('poll/:id')
+    @ApiParam({ name: 'id', description: 'Get poll by ID' })
+    async fetchPollById(@Param('id') id) {
+        return await this.mongoService.fetchPollById(id);
+    }
+
+    @Put('poll/update/:id')
+    @ApiParam({ name: 'id', description: 'ID of poll to be updated' })
+    async updatePoll(@Param('id') id, @Body() poll: UpdatePollDto) {
+        return await this.mongoService.updatePoll(id, poll);
+    }
+
+    @Get('poll/user/:author_discord_id')
+    @ApiParam({ name: 'author_discord_id', description: 'Discord user ID' })
+    async fetchPollByUser(@Param('author_discord_id') author_discord_id) {
+        return await this.mongoService.fetchPollByUser(author_discord_id);
+    }
+
+    @Get('poll/user/:author_discord_id/ongoing')
+    @ApiParam({ name: 'author_discord_id', description: 'Discord user ID' })
+    async fetchPollByUserOngoing(@Param('author_discord_id') author_discord_id) {
+        return await this.mongoService.fetchPollByUserOngoing(author_discord_id);
+    }
+
+    //
+    // @Delete('/:id')
+    // async delete(@Res() response, @Param('id') id) {
+    //     const deletedBook = await this.bookService.delete(id);
+    //     return response.status(HttpStatus.OK).json({
+    //         deletedBook
+    //     })
+    // }
+
+    // // @eslint-ignore
+    // @Post('find-one-poll')
+    // @ApiOperation({ description: 'Retrieve a poll by filter' })
+    // @ApiOkResponse({ description: 'Successfully returned poll object', type: UpdatePollDto })
+    // async findOne(
+    //     @Body() params: UpdatePollDto,
+    // ): Promise<any> {
+    //     return await this.mongoService.findOne(params);
+    // }
+    //
+    // // @eslint-ignore
+    // @Post('find-many-poll')
+    // @ApiOperation({ description: 'Retrieve a poll by filter' })
+    // @ApiOkResponse({ description: 'Successfully returned poll object', type: UpdatePollDto })
+    // async find(
+    //     @Body() params: UpdatePollDto,
+    // ): Promise<any> {
+    //     return await this.mongoService.find(params);
+    // }
+
+
 }
---
+++ src/schemas/poll.schema.ts
@@ -0,0 +1,39 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+
+export type PollDocument = Poll & Document;
+
+@Schema({ collection: 'poll', toObject: { getters: true }, optimisticConcurrency: true, timestamps: true })
+export class Poll {
+    @Prop({ required: true, type: mongoose.Schema.Types.ObjectId })
+        _id: string;
+
+    @Prop({ required: true })
+        title: string;
+
+    @Prop({ required: true })
+        channel_id: number;
+
+    @Prop({ required: false, type: Object })
+        poll_options: Record<string, any>;
+
+    @Prop({ required: false, default: false })
+        allow_options_for_anyone: boolean;
+
+    @Prop({ required: false, default: false })
+        single_vote: boolean;
+
+    @Prop({ required: true, type: mongoose.Schema.Types.Date })
+        end_time: Date;
+
+    @Prop({ required: true })
+        description: string;
+
+    @Prop({ required: false, type: [String] })
+        role_restrictions: Array<string>;
+
+    @Prop({ required: true })
+        author_discord_id: string;
+}
+
+export const PollSchema = SchemaFactory.createForClass(Poll);
