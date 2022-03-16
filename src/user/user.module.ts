98079db1876faeba5afbea7f334c12878a27d08a
tikisailor <40813246+tikisailor@users.noreply.github.com>
2022-03-16 20:31:10 +0800

    add module import

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,13 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
 import { UserMongoService } from './user.mongo.service';
 import { AccountModule } from '../account/account.module';
+import { UserController } from './user.controller';
 
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
         AccountModule,
     ],
-    controllers: [],
+    controllers: [UserController],
     providers: [UserMongoService],
 })
 export class UserModule {}
