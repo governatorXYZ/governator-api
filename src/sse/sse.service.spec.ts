d36f521118a80aafb77be84aaf329b092926d3cf
tikisailor <40813246+tikisailor@users.noreply.github.com>
2022-03-25 15:59:37 +0800

    tests for sse module

---
+++ src/sse/sse.service.spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { SseService } from './sse.service';
+import { SseController } from './sse.controller';
+import constants from '../common/constants';
+import { lastValueFrom, of } from 'rxjs';
+
+describe('Test SseService', () => {
+
+
+    let sseController: SseController;
+    let sseService: SseService;
+    beforeEach(async () => {
+
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            controllers: [SseController],
+            providers: [SseService],
+        }).compile();
+
+        sseController = moduleFixture.get<SseController>(SseController);
+        sseService = moduleFixture.get<SseService>(SseService);
+    });
+
+    it('checks if emit() pushes to event stream', async () => {
+
+        const testMsg = { data: 'test', type: constants.EVENT_POLL_CREATE };
+
+        // await sseController.publish(testMsg);
+        await sseService.emit(testMsg);
+
+        jest.spyOn(sseController, 'stream').mockImplementationOnce(() => of (testMsg));
+
+        await expect(lastValueFrom(sseController.stream()))
+            .resolves.toEqual(testMsg);
+    });
+});
