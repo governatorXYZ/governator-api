fe332f0a79ac21939e34e2daea4a8602e58be0fc
tikisailor <40813246+tikisailor@users.noreply.github.com>
2022-05-07 12:00:00 +0800

    adding db name to env (#18)

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,7 +28,7 @@ const ENV = process.env.NODE_ENV;
         MongooseModule.forRootAsync({
             imports: [ConfigModule],
             useFactory: async (configService: ConfigService) => ({
-                uri: `${configService.get('MONGODB_PREFIX')}://${configService.get('MONGODB_USERNAME')}:${configService.get('MONGODB_PASS')}@${configService.get('MONGODB_CLUSTER')}/governator`,
+                uri: `${configService.get('MONGODB_PREFIX')}://${configService.get('MONGODB_USERNAME')}:${configService.get('MONGODB_PASS')}@${configService.get('MONGODB_CLUSTER')}/${configService.get('MONGODB_DATABASE')}`,
             }),
             inject: [ConfigService],
         }),
