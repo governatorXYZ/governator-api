a89517f12572a4ba8e5af0c51ae9e9524b971bff
tikisailor <40813246+tikisailor@users.noreply.github.com>
2022-11-09 10:27:29 -0700

    added non token strategy (always returns 1)

--- src/web3/strategy/strategies/CONFIG.ts
+++ src/web3/strategy/strategies/CONFIG.ts
@@ -2,5 +2,6 @@ export default Object.freeze({
     API_TAG: 'Strategies',
     API_URL_BASE: 'strategies',
     STRATEGY_BANKLESS_DAO: 'Bankless Dao',
+    STRATEGY_NONE: 'Empty strategy (returns voting power of 1)',
     // add strategy names here
 });
---
+++ src/web3/strategy/strategies/controller.noneStrategy.ts
@@ -0,0 +1,66 @@
+import { Body, Controller, HttpCode, HttpStatus, OnApplicationBootstrap, Post } from '@nestjs/common';
+import { ApiBody, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { StrategyBaseController } from '../strategy.base.controller';
+import { StrategyBaseService } from '../strategy.base.service';
+import { StrategyMongoService } from '../strategy.mongo.service';
+import * as path from 'path';
+import { StrategyRequestDto } from '../strategy.dtos';
+import { formatKebab } from '../strategy.utils';
+import apiConfig from './CONFIG';
+
+const conf = {
+    api_tag: apiConfig.API_TAG,
+    api_url_base: apiConfig.API_TAG.toLowerCase(),
+    // modify to match your startegy setting in CONFIG.ts
+    name: apiConfig.STRATEGY_NONE,
+};
+
+@ApiTags(conf.api_tag)
+@Controller(conf.api_url_base)
+export class NoneStrategy extends StrategyBaseController implements OnApplicationBootstrap {
+    constructor(
+        protected strategyService: StrategyBaseService,
+        protected strategyMongoService: StrategyMongoService,
+    ) {
+        super(strategyService, strategyMongoService);
+    }
+
+    // modify: implement strategy here
+    async strategy(
+    ) {
+        return '1';
+    }
+
+    // transform strategy result, or use to chain strategies
+    responseTransformer(
+        strategyResult: any,
+    ): string {
+        return strategyResult;
+
+    }
+
+    // do not modify
+    _getConf() {
+        return conf;
+    }
+
+    // do not modify
+    _getFileName() {
+        return path.basename(__filename);
+    }
+
+    // do not modify
+    @Post(formatKebab(conf.name))
+    @ApiBody({ type: StrategyRequestDto })
+    @ApiOperation({ description: conf.name })
+    @ApiOkResponse({ description: 'Returns a users voting power under this strategy', type: String, isArray: false })
+    @HttpCode(HttpStatus.OK)
+    async post(
+        @Body() params: StrategyRequestDto): Promise<string> {
+        return (await super.runStrategy(
+            params,
+            this.strategy,
+            this.responseTransformer,
+        ));
+    }
+}
