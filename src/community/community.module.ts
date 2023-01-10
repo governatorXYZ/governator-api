f2490bb7ad6a0dd3db51b2df78d94f877adf78b8
tikisailor <40813246+tikisailor@users.noreply.github.com>
2023-01-10 14:52:14 -0700

    pre save hook works but not for what I wanted to do. Maybe use some other time

--- src/community/community.module.ts
+++ src/community/community.module.ts
@@ -1,12 +1,53 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { HttpException, Module, HttpStatus, forwardRef } from '@nestjs/common';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CommunityMongoService } from './community.mongo.service';
-import { Community, CommunitySchema } from './community.schema';
+import { Community, CommunitySchema, CommunityDocument } from './community.schema';
 import { CommunityController } from './community.controller';
+import { CommunityClientConfigDiscordDto } from './community.dtos';
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{ name: Community.name, schema: CommunitySchema }]),
+        MongooseModule.forFeatureAsync([
+            {
+                name: Community.name,
+                useFactory: () => {
+                    const schema = CommunitySchema;
+          
+                    schema.pre<Community>('save', async function() {
+                        console.log('here0');
+                        try {
+                            console.log('here1');
+                            console.log(this.client_config.find((config) => config.provider_id === 'discord'));
+                            // const exists = await communityModel.fetchCommunityByDiscordGuildId((this.client_config.find((config) => config.provider_id === 'discord') as CommunityClientConfigDiscordDto).guild_id);
+// @ts-ignore
+                            const exists = await this.constructor.findOne(
+                                {
+                                    client_config: {
+                                        '$elemMatch': {
+                                            provider_id: 'discord',
+                                            guild_id: (this.client_config.find((config) => config.provider_id === 'discord') as CommunityClientConfigDiscordDto).guild_id,
+                                        },
+                                    },
+                                },
+                            ).exec();
+
+                            console.log(exists);
+
+                            if (exists) {
+                                throw new Error;
+                            }
+
+                        } catch (err) {
+                            // do nothing
+                        }
+                    });
+
+                    return schema;
+                },
+                // inject: [CommunityMongoService],
+            },
+        ]),
     ],
     controllers: [CommunityController],
     providers: [CommunityMongoService],
