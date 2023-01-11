21d53415ef3e0105780191b493674354c64f5560
tikisailor <40813246+tikisailor@users.noreply.github.com>
2023-01-10 18:46:57 -0700

    add validation for discord guild id

--- src/community/community.module.ts
+++ src/community/community.module.ts
@@ -1,10 +1,13 @@
-import { HttpException, Module, HttpStatus, forwardRef } from '@nestjs/common';
+import { Logger, Module, HttpStatus, forwardRef, CACHE_MANAGER } from '@nestjs/common';
 import { MongooseModule, getModelToken } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CommunityMongoService } from './community.mongo.service';
 import { Community, CommunitySchema, CommunityDocument } from './community.schema';
 import { CommunityController } from './community.controller';
 import { CommunityClientConfigDiscordDto } from './community.dtos';
+import { Cache } from 'cache-manager';
+import { existsSync } from 'fs';
+
 
 @Module({
     imports: [
@@ -13,14 +16,14 @@ import { CommunityClientConfigDiscordDto } from './community.dtos';
                 name: Community.name,
                 useFactory: () => {
                     const schema = CommunitySchema;
-          
-                    schema.pre<Community>('save', async function() {
-                        console.log('here0');
+
+                    const logger = new Logger('MongooseModuleCommunity');
+
+                    schema.pre<Community>('validate', async function() {
+                        logger.log('Running mongoose pre-validate hook');
                         try {
-                            console.log('here1');
-                            console.log(this.client_config.find((config) => config.provider_id === 'discord'));
-                            // const exists = await communityModel.fetchCommunityByDiscordGuildId((this.client_config.find((config) => config.provider_id === 'discord') as CommunityClientConfigDiscordDto).guild_id);
-// @ts-ignore
+                            // We fetch document with same guild_id in CommunityClientConfigDiscord from db
+                            // @ts-ignore
                             const exists = await this.constructor.findOne(
                                 {
                                     client_config: {
@@ -31,21 +34,33 @@ import { CommunityClientConfigDiscordDto } from './community.dtos';
                                     },
                                 },
                             ).exec();
-
-                            console.log(exists);
-
+                            
+                            // if the document exists we have a duplicate entry and should not allow it to be saved
                             if (exists) {
-                                throw new Error;
+
+                                logger.error(
+                                    `Duplicate discord guild ID found in community config.
+                                    EXISTING Community object_id: ${exists._id}
+                                    with discord client config: ${JSON.stringify(exists.client_config.find((config) => config.provider_id === 'discord'))}.
+                                    NEW Community object_id: ${this._id}
+                                    with discord client config: ${JSON.stringify(this.client_config.find((config) => config.provider_id === 'discord'))}.
+                                    Triggering duplicate key error.`);
+                                
+                                // since there seems to be no way to cancel the transaction or to update the validation function
+                                // we update the new documents ID to equal existing documents ID which will trigger a validation 
+                                // error and prevent document from being saved.
+                                // @ts-ignore
+                                this.set('_id', exists._id);
                             }
 
                         } catch (err) {
-                            // do nothing
+                            logger.log('No conflicting documents found, commencing validation.');
                         }
                     });
 
                     return schema;
                 },
-                // inject: [CommunityMongoService],
+                // inject: [CACHE_MANAGER],
             },
         ]),
     ],
