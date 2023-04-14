48d8306dbbe65143ce695c4383313947bf6ec550
tikisailor <40813246+tikisailor@users.noreply.github.com>
2023-04-14 12:24:02 +0800

    testing

--- src/app.config.ts
+++ src/app.config.ts
@@ -33,19 +33,20 @@ export const configure = (app, setupSwaggerModule = true): OpenAPI.Document => {
     if (configService.get('NODE_ENV') === 'development') {
         app.enableCors();
     } else if (configService.get('NODE_ENV') === 'production') {
-        app.enableCors({ origin: configService.get('CORS_ORIGIN').split(' ') });
+        // app.enableCors({ origin: configService.get('CORS_ORIGIN').split(' ') });
+        app.enableCors();
     }
 
     // Put a helmet on
     app.use(helmet());
 
     // specify cors and credentials for oauth session with FE
-    app.use(function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', configService.get('FE_HOST'));
-        res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-        next();
-    });
+    // app.use(function(req, res, next) {
+    //     res.header('Access-Control-Allow-Origin', configService.get('FE_HOST'));
+    //     res.header('Access-Control-Allow-Credentials', true);
+    //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    //     next();
+    // });
 
     // use global auth guard
     // const reflector = app.get(Reflector);
