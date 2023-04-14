bf3a87d9df2c3aa3218bb3cd8a1befa259344d9f
tikisailor <40813246+tikisailor@users.noreply.github.com>
2023-04-14 12:40:30 +0800

    testing

--- src/app.config.ts
+++ src/app.config.ts
@@ -44,6 +44,7 @@ export const configure = (app, setupSwaggerModule = true): OpenAPI.Document => {
     app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Origin', configService.get('FE_HOST'));
         res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         next();
     });
