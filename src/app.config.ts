8eb42192e66097609aa30fd543ab145ab3a7d91f
tikisailor <40813246+tikisailor@users.noreply.github.com>
2023-04-14 12:30:45 +0800

    testing

--- src/app.config.ts
+++ src/app.config.ts
@@ -30,23 +30,23 @@ export const configure = (app, setupSwaggerModule = true): OpenAPI.Document => {
     }));
 
     // set cors origin policy from .env
-    if (configService.get('NODE_ENV') === 'development') {
-        app.enableCors();
-    } else if (configService.get('NODE_ENV') === 'production') {
-        // app.enableCors({ origin: configService.get('CORS_ORIGIN').split(' ') });
-        app.enableCors(configService.get('FE_HOST'));
-    }
+    // if (configService.get('NODE_ENV') === 'development') {
+    //     app.enableCors();
+    // } else if (configService.get('NODE_ENV') === 'production') {
+    //     // app.enableCors({ origin: configService.get('CORS_ORIGIN').split(' ') });
+    //     app.enableCors(configService.get('FE_HOST'));
+    // }
 
     // Put a helmet on
     app.use(helmet());
 
     // specify cors and credentials for oauth session with FE
-    // app.use(function(req, res, next) {
-    //     res.header('Access-Control-Allow-Origin', configService.get('FE_HOST'));
-    //     res.header('Access-Control-Allow-Credentials', true);
-    //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    //     next();
-    // });
+    app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', configService.get('FE_HOST'));
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
+    });
 
     // use global auth guard
     // const reflector = app.get(Reflector);
