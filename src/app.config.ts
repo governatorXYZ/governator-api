cb7db7b2c001e4610fe213e7b1179e61908b3bfa
tikisailor <40813246+tikisailor@users.noreply.github.com>
2023-04-15 14:01:09 +0800

    testing

--- src/app.config.ts
+++ src/app.config.ts
@@ -42,7 +42,7 @@ export const configure = (app, setupSwaggerModule = true): OpenAPI.Document => {
     app.use(helmet());
 
     app.use(
-        '/api/auth',
+        '/api',
         createProxyMiddleware({
             target: configService.get('FE_HOST'),
             changeOrigin: true,
