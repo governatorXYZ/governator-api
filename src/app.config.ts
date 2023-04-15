cd90f25962e114b926c556d84023349d277c6673
tikisailor <40813246+tikisailor@users.noreply.github.com>
2023-04-15 15:05:48 +0800

    testing

--- src/app.config.ts
+++ src/app.config.ts
@@ -55,6 +55,7 @@ export const configure = (app, setupSwaggerModule = true): OpenAPI.Document => {
     app.use(
         '/api/auth/redirect',
         createProxyMiddleware({
+            target: configService.get('FE_HOST'),
             hostRewrite: configService.get('FE_HOST'),
             // cookieDomainRewrite: {
             //     'governator-api-test.herokuapp.com': configService.get('FE_HOST'),
